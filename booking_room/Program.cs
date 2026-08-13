4ed0ac7f9a9e4a0db0525e2c30d0418ecaefaa8c
ANGGORO RAVI <whyuravi.2008@gmail.com>
2026-08-13 11:17:21 +0700

    fix(di): registrasikan IFileProvider agar InlineSvg tidak crash

--- booking_room/Program.cs
+++ booking_room/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.Extensions.FileProviders;
 using booking_room.Components;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,6 +6,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddSingleton<IFileProvider>(sp =>
+    sp.GetRequiredService<IWebHostEnvironment>().WebRootFileProvider);
 
 var app = builder.Build();
 
