15c735dd28f218469e6e2dabdea113f7090f54d2
ANGGORO RAVI <whyuravi.2008@gmail.com>
2026-08-14 09:29:14 +0700

    Hapus chart analitik, tambah popup profil user pada avatar header

--- booking_room/obj/Debug/net10.0/booking_room.AssemblyInfo.cs
+++ booking_room/obj/Debug/net10.0/booking_room.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("booking_room")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4c4da0d79f0a8051811fa5c53d0f09bd75d602a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+484d88243691e327d0c5edfb8c53dedd09ff4f0b")]
 [assembly: System.Reflection.AssemblyProductAttribute("booking_room")]
 [assembly: System.Reflection.AssemblyTitleAttribute("booking_room")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
