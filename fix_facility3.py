33b3c56a5378dc20776254c6bf18fa477c962149
ANGGORO RAVI <whyuravi.2008@gmail.com>
2026-09-03 09:54:01 +0700

    revisi V33

--- fix_facility3.py
+++
@@ -1,8 +0,0 @@
-﻿import os
-
-path = 'booking_room/Components/Shared/FacilityRequestView.razor'
-with open(path, 'r', encoding='utf-8') as f: content = f.read()
-
-content = content.replace("FacilityRequestItem", "FacilityRequest")
-with open(path, 'w', encoding='utf-8') as f: f.write(content)
-print("Fixed FacilityRequest type.")
