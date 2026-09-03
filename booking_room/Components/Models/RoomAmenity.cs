b2abeb47c0d007c2264b492ea2e37b972119371c
ANGGORO RAVI <whyuravi.2008@gmail.com>
2026-09-03 11:04:38 +0700

    revisi V38: localize UI to Bahasa Indonesia, fix all popup click bugs, dynamic amenities with SVG icons, redesign UI

---
+++ booking_room/Components/Models/RoomAmenity.cs
@@ -0,0 +1,14 @@
+﻿namespace booking_room.Components.Models;
+
+public class RoomAmenity
+{
+    public string Name { get; set; } = "";
+    public string IconSvg { get; set; } = "";
+}
+
+public class Room
+{
+    public string Id { get; set; } = "";
+    public string Name { get; set; } = "";
+    public List<RoomAmenity> Amenities { get; set; } = new();
+}
---
+++ booking_room/Resources/RoomSearch.cs
@@ -0,0 +1,16 @@
+﻿namespace booking_room.Resources;
+
+public static class RoomSearch
+{
+    public static string SearchRoomsTitle => "CARI RUANGAN";
+    public static string SearchPlaceholder => "Cari berdasarkan nama ruangan, gedung, atau lantai...";
+    public static string DateLabel => "TANGGAL";
+    public static string TimeAndDurationLabel => "WAKTU & DURASI";
+    public static string AttendeesLabel => "PESERTA";
+    public static string Available => "TERSEDIA";
+    public static string Booked => "DIPESAN (2 JAM LAGI)";
+    public static string Unavailable => "TIDAK TERSEDIA";
+    public static string ViewDetails => "LIHAT DETAIL";
+    public static string SelectDate => "PILIH TANGGAL";
+    public static string SelectTime => "PILIH WAKTU";
+}
